4b3eb8f3e2afe45f6a8539731b667dd5832b8f42
Sirmikky <michael.agbogo@echithub.com>
2022-04-28 16:27:41 +0100

    Map permissions completed. Get Geocode completed

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -3,54 +3,40 @@ package com.udacity.project4.locationreminders.savereminder.selectreminderlocati
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Activity
-import android.app.Activity.RESULT_CANCELED
-import android.app.Activity.RESULT_OK
-import android.content.Intent
-import android.content.IntentSender
+import android.app.AlertDialog
 import android.content.pm.PackageManager
-import android.content.res.Resources
-import android.os.Build
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
-import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
-import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
-import com.udacity.project4.utils.Constants
+import com.udacity.project4.utils.Constants.PERMISSION_BACKGROND_LOCATION_REQUEST_CODE
 import com.udacity.project4.utils.Constants.PERMISSION_LOCATION_REQUEST_CODE
-import com.udacity.project4.utils.Permissions.hasBackgroundLocationPermission
 import com.udacity.project4.utils.Permissions.hasLocationPermission
-import com.udacity.project4.utils.Permissions.requestBackgroundLocationPermission
-import com.udacity.project4.utils.Permissions.requestLocationPermission
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
-import com.vmadalin.easypermissions.EasyPermissions
-import com.vmadalin.easypermissions.annotations.AfterPermissionGranted
-import com.vmadalin.easypermissions.dialogs.SettingsDialog
-import org.koin.android.ext.android.bind
 import org.koin.android.ext.android.inject
-import java.security.Permissions
+import java.io.IOException
 
 class SelectLocationFragment : BaseFragment(), GoogleMap.OnMarkerClickListener,
-    GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnPoiClickListener,
-    EasyPermissions.PermissionCallbacks, EasyPermissions.RationaleCallbacks {
+    GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnPoiClickListener {
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
@@ -61,11 +47,12 @@ class SelectLocationFragment : BaseFragment(), GoogleMap.OnMarkerClickListener,
     private lateinit var intendedLatLong: LatLng
     private lateinit var selectedReminder: ReminderDTO
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
         mMap = googleMap
         setUpObservers()
-        enableMyLocation()
         // Create a Geofence instance
         geofencingClient = LocationServices.getGeofencingClient(requireContext())
 
@@ -77,49 +64,19 @@ class SelectLocationFragment : BaseFragment(), GoogleMap.OnMarkerClickListener,
             isMapToolbarEnabled = false
             isZoomControlsEnabled = true
             isZoomGesturesEnabled = true
+
         }
-        setMapStyle(mMap)
-//        addObservers()
+        checkLocationPermission()
         mMap.setOnMarkerClickListener(this)
         mMap.setOnPoiClickListener(this)
         mMap.setOnMyLocationButtonClickListener(this)
         onMapClicked()
         onMapLongClick()
+        setMapStyle(mMap)
 //        addListeners()
-//        displayInfoMessage()
 
     }
 
-    @SuppressLint("MissingPermission")
-    private fun enableMyLocation() {
-        if (!hasLocationPermission(requireContext())) {
-            _viewModel.hasPermission.postValue(false)
-            requestLocationPermission(this)
-        } else {
-            _viewModel.hasPermission.postValue(true)
-            mMap.isMyLocationEnabled = true
-        }
-    }
-
-    private fun setUpObservers(){
-        _viewModel.hasPermission.observe(viewLifecycleOwner, Observer { hasPermission ->
-            Log.i("Permission",hasPermission.toString())
-            if (hasPermission){
-                binding.btnRequestPermission.visibility = View.GONE
-            }else{
-                binding.btnRequestPermission.visibility = View.VISIBLE
-            }
-
-        })
-    }
-
-    private fun navigateBackIfLocationIsNotGranted() {
-        if (!hasLocationPermission(requireContext())) {
-            findNavController().popBackStack()
-        }
-    }
-
-    @SuppressLint("MissingPermission")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
@@ -136,22 +93,152 @@ class SelectLocationFragment : BaseFragment(), GoogleMap.OnMarkerClickListener,
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
 //        TODO: zoom to the user location after taking his permission
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
 //        TODO: add style to the map
 //        TODO: put a marker to location that the user selected
 
 
 //        TODO: call this function after the user confirms on the selected location
-        onLocationSelected()
+
         binding.btnRequestPermission.setOnClickListener {
-            if (!hasLocationPermission(requireContext())) {
-                requestLocationPermission(this)
-            }else{
-                _viewModel.hasPermission.postValue(true)
-                mMap.isMyLocationEnabled = true
-            }
+            checkLocationPermission()
         }
 
         return binding.root
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun setUpObservers() {
+        _viewModel.hasPermission.observe(viewLifecycleOwner, Observer { hasPermission ->
+            Log.i("Permission", hasPermission.toString())
+            if (hasPermission) {
+                mMap.isMyLocationEnabled = true
+                binding.btnRequestPermission.visibility = View.GONE
+                zoomIntoLastKnowPostion()
+            } else {
+                binding.btnRequestPermission.visibility = View.VISIBLE
+            }
+
+        })
+    }
+
+
+    private fun checkLocationPermission() {
+        val permission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                val builder = AlertDialog.Builder(requireContext())
+                builder
+                    .setMessage(getString(R.string.rational_mesasge))
+                    .setTitle("Permission Required")
+                    .setPositiveButton("OK") { dialog, id ->
+                        makePermissionRequest()
+                    }.setNegativeButton("Cancel") { dialog, id ->
+                        Log.i("Permission", "Location Permission Cancel")
+                        findNavController().popBackStack()
+                    }
+
+                val dialog = builder.create().show()
+
+            } else {
+                makePermissionRequest()
+            }
+        } else {
+            Log.i("Permission", "Location Permission Granted")
+            _viewModel.hasPermission.postValue(true)
+
+        }
+    }
+
+    private fun checkBackgroundPermission() {
+        val permission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        )
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                )
+            ) {
+                val builder = AlertDialog.Builder(requireContext())
+                builder
+                    .setMessage(getString(R.string.rational_mesasge))
+                    .setTitle("Permission Required")
+                    .setPositiveButton("OK") { dialog, id ->
+                        makeBackgroundPermissionRequest()
+                    }.setNegativeButton("Cancel") { dialog, id ->
+                        Toast.makeText(requireContext(), "Permission Needed", Toast.LENGTH_LONG)
+                    }
+
+                val dialog = builder.create().show()
+
+            } else {
+                makeBackgroundPermissionRequest()
+            }
+        } else {
+            // Performs some action with background location
+            Log.i("Permission", "Backgroud Permission Granted")
+        }
+    }
+
+
+    private fun makePermissionRequest() {
+        requestPermissions(
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_LOCATION_REQUEST_CODE
+        )
+    }
+
+    private fun makeBackgroundPermissionRequest() {
+        requestPermissions(
+            arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+            PERMISSION_BACKGROND_LOCATION_REQUEST_CODE
+        )
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_LOCATION_REQUEST_CODE -> {
+
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    findNavController().popBackStack()
+                } else {
+                    Log.i("Permission", "Permission Granted")
+                    _viewModel.hasPermission.postValue(true)
+                }
+            }
+            PERMISSION_BACKGROND_LOCATION_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("Permission", "Permission Not Granted")
+                } else {
+                    Log.i("Permission", "Permission Granted")
+                }
+            }
+        }
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun zoomIntoLastKnowPostion() {
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                val position = location?.let { LatLng(it.latitude, it.longitude) }
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 16f))
+            }
     }
 
 
@@ -169,54 +256,53 @@ class SelectLocationFragment : BaseFragment(), GoogleMap.OnMarkerClickListener,
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         // TODO: Change the map type based on the user's selection.
         R.id.normal_map -> {
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
             true
         }
         R.id.hybrid_map -> {
+            mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
             true
         }
         R.id.satellite_map -> {
+            mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
             true
         }
         R.id.terrain_map -> {
+            mMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
             true
         }
         else -> super.onOptionsItemSelected(item)
     }
 
     override fun onMarkerClick(p0: Marker?): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 
     override fun onMyLocationButtonClick(): Boolean {
-        if (!hasLocationPermission(requireContext())) {
-            Log.i("Permission", "onMyLocationButtonClick No Permission ")
-            requestLocationPermission(this)
-            return false
-        }
-        _viewModel.hasPermission.postValue(true)
+        zoomIntoLastKnowPostion()
         return false
     }
 
-    override fun onPoiClick(p0: PointOfInterest?) {
-        TODO("Not yet implemented")
+    override fun onPoiClick(poi: PointOfInterest?) {
+        Log.i("POI", poi?.latLng?.latitude.toString())
+        Log.i("POI", poi?.latLng?.longitude.toString())
+        Log.i("POI", poi?.name.toString())
+        Log.i("POI", poi?.placeId.toString())
     }
 
     private fun onMapClicked() {
         mMap.setOnMapClickListener {
-
+            makePermissionRequest()
         }
     }
 
+    /*
+    on Map Long Click
+     */
     private fun onMapLongClick() {
         mMap.setOnMapLongClickListener {
-            intendedLatLong = it
-            if (hasBackgroundLocationPermission(requireContext())) {
-                Log.i("Permission", "onMapClicked has Permission ")
-            } else {
-                Log.i("Permission", "onMapClicked No Permission ")
-                requestBackgroundLocationPermission(this)
-            }
-
+            checkBackgroundPermission()
+            reverseGeocodeLocation(it.latitude,it.longitude)
         }
     }
 
@@ -247,74 +333,8 @@ class SelectLocationFragment : BaseFragment(), GoogleMap.OnMarkerClickListener,
         }
     }
 
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        Log.i("Permission",requestCode.toString())
-//        if (requestCode == Constants.REQUEST_TURN_DEVICE_LOCATION_ON) {
-//
-//        }
-//
-//    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (resultCode == RESULT_CANCELED){
-            Log.i("Permission", "onActivityResult Cancel Button")
-        }
-        if (requestCode == PERMISSION_LOCATION_REQUEST_CODE && resultCode == RESULT_OK) {
-            // Do something after user returned from app settings screen, like showing a Toast.
-            Toast.makeText(requireContext(), "From App Settings", Toast.LENGTH_SHORT)
-                .show();
-            _viewModel.hasPermission.postValue(true)
-        }
-        if (requestCode == PERMISSION_LOCATION_REQUEST_CODE && resultCode == RESULT_CANCELED) {
-            // Do something after user returned from app settings screen, like showing a Toast.
-            requestLocationPermission(this)
-        }
-
-    }
-
-    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
-        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            Log.i("Permission", "onPermissionDenied If")
-            SettingsDialog.Builder(requireActivity()).build().show()
-        }
-//        else if (EasyPermissions.somePermissionDenied(this,Manifest.permission.ACCESS_FINE_LOCATION)){
-//            Log.i("Permission", "onPermissionDenied Specific")
-//            SettingsDialog.Builder(requireActivity()).build().show()
-//        }
-        else {
-            if (requestCode == PERMISSION_LOCATION_REQUEST_CODE) {
-                Log.i("Permission", "onPermissionDenied Else")
-//                findNavController().popBackStack()
-            }
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
-        Log.i("Permission", "onPermissionGranted ")
-        _viewModel.hasPermission.postValue(true)
-        mMap.isMyLocationEnabled = true
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSION_LOCATION_REQUEST_CODE) {
-            if (hasLocationPermission(requireContext())){
-                _viewModel.hasPermission.postValue(true)
-            }else{
-                requestLocationPermission(this)
-            }
-        }
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
-    }
-
-    private fun setMapStyle(resource: Int) {
+    private fun updateMapStyle(resource: Int) {
         try {
             mMap.setMapStyle(
                 MapStyleOptions.loadRawResourceStyle(
@@ -327,13 +347,33 @@ class SelectLocationFragment : BaseFragment(), GoogleMap.OnMarkerClickListener,
         }
     }
 
-    override fun onRationaleAccepted(requestCode: Int) {
-        Log.i("Permission", "onRationaleAccepted ")
-        _viewModel.hasPermission.postValue(true)
-    }
+    @SuppressLint("MissingPermission")
+    private fun reverseGeocodeLocation(latitude: Double,longitude: Double) {
+        var geocodeMatches: List<Address>? = null
+        val address1: String?
+        val address2: String?
+        val state: String?
+        val zipCode: String?
+        val Country: String?
 
-    override fun onRationaleDenied(requestCode: Int) {
-        Log.i("Permission", "onRationaleDenied ")
+        try {
+            geocodeMatches =
+                Geocoder(requireContext()).getFromLocation(latitude, longitude, 1)
+        }catch (e:IOException){
+            e.printStackTrace()
+        }
+
+
+        geocodeMatches.let {
+            address1 = it?.get(0)?.getAddressLine(0)
+            address2 = it?.get(0)?.getAddressLine(1)
+            state = it?.get(0)?.adminArea
+            zipCode = it?.get(0)?.postalCode
+            Country = it?.get(0)?.countryName
+        }
+
+        Log.i("Geocode",geocodeMatches.toString())
+
     }
 
 
